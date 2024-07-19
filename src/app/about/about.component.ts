5081458c30ac2f1910d10efaa3c7478c9f965ac8
alex <aleksandra.vito@lhind.dlh.de>
2024-07-19 16:48:24 +0200

    #6 - what is rxjs - what problem does it solve

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -28,17 +28,18 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
     document.addEventListener('click', evt => {
-      console.log({evt})
+      console.log({evt});
+      setTimeout(() => {
+        console.log(' ... finished');
+        let counter = 0;
+        setInterval(() => {
+          console.log({counter});
+          counter++;
+        }, 1000);
+      }, 3000);
     })
-    let counter = 0;
-    setInterval(() => {
-      console.log({counter});
-      counter++;
-    }, 1000);
 
-    setTimeout(() => {
-      console.log(' ... finished');
-    }, 3000)
+
   }
 
 
